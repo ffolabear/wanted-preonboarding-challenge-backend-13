8b06751eeb226bc83a04affe231dc067bc319b62
ffolabear <ffolabear@gmail.com>
2023-09-04 14:22:21 +0900

    refactor: theater 패키지 코드 포맷 수정

--- src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/TicketSeller.java
@@ -1,13 +1,13 @@
 package com.wanted.preonboarding.theater.service.handler;
 
+import lombok.Getter;
+
+@Getter
 public class TicketSeller {
     private final TicketOffice ticketOffice;
 
-    public TicketSeller(TicketOffice ticketOffice){
+    public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice(){
-        return ticketOffice;
-    }
 }
